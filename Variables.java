40757d154593ce4e9f1475db7565b5edc91ea8e6
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-09 21:42:35 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,24 @@
+
+public class Variables {
+	public static void main (String[] args) {
+		int a = 42;
+		double aa = 4.2;
+		long aaa = 4200;
+		float aaaa = 5.51f;
+		char aaaaa = 'b' ;
+		boolean b = true ;
+		byte bb = 127;
+		
+		System.out.println(a);
+		System.out.println(aa);
+		System.out.println(aaa);
+		System.out.println(aaaa);
+		System.out.println(aaaaa);
+		System.out.println(b);
+		System.out.println(bb);
+		
+				
+		
+	}
+
+}
