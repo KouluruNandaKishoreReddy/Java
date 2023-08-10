7e4a0649b34ea0f15ebb5bf62f8da14561562f92
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-10 19:09:55 +0530

    Add files via upload

---
+++ GettingUsernput.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+public class GettingUsernput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner nanda = new Scanner(System.in);
+		System.out.print("enter the text "+nanda.nextLine());
+	
+		
+
+	}
+
+}
