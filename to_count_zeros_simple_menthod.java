96ece9704947c18090d6b852eec7fcc40a07088e
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-12 17:37:51 +0530

    Add files via upload

---
+++ to_count_zeros_simple_menthod.java
@@ -0,0 +1,14 @@
+
+public class to_count_zeros_simple_menthod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// to count we can use _ between zeros the output will be the same
+		int a = 10_00_00_000;
+		System.out.println(a);
+		
+
+	}
+
+}
