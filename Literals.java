92977ada17c8c347ceda4471cbcdb0170caad076
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-12 17:37:30 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,18 @@
+
+public class Literals {
+
+	public static void main(String[] args) {
+		// Literals means the assign value 
+		 
+			// To use binary   use 0b
+		int a = 0b101;
+		 System.out.println(a);
+		 
+		 // To use hexadecimal use 0x
+		 int b =  0x7E;
+		 System.out.println(b);
+		 
+		 
+	}
+
+}
