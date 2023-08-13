3fe21ffb95d87ed3f4d38fe5904f5e405f3d10fa
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-13 20:55:27 +0530

    Add files via upload

---
+++ relation_operators.java
@@ -0,0 +1,20 @@
+
+public class relation_operators {
+
+	public static void main(String[] args) {
+		// relation operators are <,>,==,>=,<=
+		
+		int a = 8;
+		int b = 6;
+		
+		
+	    boolean result = a < b; // Like this we can use <,>, == , >=, <=
+	    System.out.println(result);
+	    
+		 
+				 
+		
+
+	}
+
+}
