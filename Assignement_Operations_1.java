cdb692a3fc25388529935d03f79f33860b118444
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-13 19:47:23 +0530

    Add files via upload

---
+++ Assignement_Operations_1.java
@@ -0,0 +1,32 @@
+
+public class Assignement_Operations_1 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 8;
+		
+		// addition
+		int result = a + b;
+		System.out.println(result);
+		
+		// subtraction
+		int result1 = a - b;
+		System.out.println(result1);
+		
+		// multiplication
+		
+		int result2 = a * b;
+		System.out.println(result2);
+		
+		// Division to get quotient
+		int result3 = a / b;
+		System.out.println(result3);
+		
+		// modulus to get reminder
+		int result4 = a % b;
+		System.out.println(result4);
+		
+
+	}
+
+}
