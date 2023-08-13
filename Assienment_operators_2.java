3bbca901c17613f1b0ecdfc231d900d8711acbd9
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-13 19:48:21 +0530

    Add files via upload

---
+++ Assienment_operators_2.java
@@ -0,0 +1,22 @@
+                               // Arithmetic operations
+public class Assienment_operators_2 {
+
+	public static void main(String[] args) {
+		
+		int a = 8;
+		
+		// Different types to increment the value 
+		a=a+1;                  // 8=8+1 == 9
+		a++; // post increment  // 9+1 == 10
+		++a; // pre increment   // 1+10 == 11
+		a+=1;                   // 11+=1 == 12
+		
+		// so a is 12 same like this we can use for decrement also
+		
+		System.out.println(a);
+		
+		
+
+	}
+
+}
