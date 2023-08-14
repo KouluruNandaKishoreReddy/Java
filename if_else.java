3c64ec2b082595f01753eff818cba7cd34cc04db
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-14 19:25:50 +0530

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,14 @@
+
+public class if_else {
+
+	public static void main(String[] args) {
+		int x=10;
+		
+		if (x>=10 & x <= 20)
+			System.out.println("hai");
+		else
+			System.out.println("bye");
+
+	}
+
+}
