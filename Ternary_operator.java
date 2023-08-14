aced19c3015eae8d45b1cb9453e87a21299a4d7b
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-14 19:53:03 +0530

    Add files via upload

---
+++ Ternary_operator.java
@@ -0,0 +1,15 @@
+
+public class Ternary_operator {
+
+	public static void main(String[] args) {
+		// ternary operator will be indicated by using ?:
+		// after writing the literals if true execute the question mark part else execute quotation part
+		int n=4;
+		int result=0;
+		
+		result=n%2==0 ?10 : 20;
+		System.out.println(result);
+
+	}
+
+}
