9d64deaf01b01837da0e1c7b68b0bc84bd2ab2de
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-14 19:32:02 +0530

    Add files via upload

---
+++ if_else_1.java
@@ -0,0 +1,20 @@
+
+public class if_else_1 {
+
+	public static void main(String[] args) {
+		// if has more than 2 statements we need to use brackets {}
+		
+		int x=10;
+		int y=8;
+		
+		if(x>y)
+		{
+			System.out.println("x is great");
+			System.out.println(x);
+		}
+		else
+			System.out.println("y is greater");
+
+	}
+
+}
