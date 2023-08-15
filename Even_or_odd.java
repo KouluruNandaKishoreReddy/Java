95743e6a0e2cfa4222280eb927dc3d4c1085c39e
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-15 19:40:02 +0530

    Add files via upload

---
+++ Even_or_odd.java
@@ -0,0 +1,16 @@
+
+public class Even_or_odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=9;
+		if (n%2==0)
+		{
+			System.out.println("Even Number " + n);
+		}
+		else
+			System.out.println("Odd Number " + n);
+
+	}
+
+}
