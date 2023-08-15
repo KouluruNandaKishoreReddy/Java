e720350ca472de26563c0a210d200ba54f3816ce
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-15 19:50:24 +0530

    Add files via upload

---
+++ Print_even_numbers_1_to_100.java
@@ -0,0 +1,18 @@
+
+public class Print_even_numbers_1_to_100 {
+
+	public static void main(String[] args) {
+		
+		int n = 100;
+		
+		for (int i=1; i<=n;i++)
+		{
+			if(i%2==0)
+			System.out.println(i); // for even
+			
+			// System.out.println(i-1) for odd
+		}
+
+	}
+
+}
