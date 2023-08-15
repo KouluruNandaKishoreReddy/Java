8dcaa9310a0aa3e07212289cc0085afd55e5ded1
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-15 17:26:42 +0530

    Add files via upload

---
+++ do_while_loop.java
@@ -0,0 +1,16 @@
+
+public class do_while_loop {
+
+	public static void main(String[] args) {
+		// Do statement will execute at least once if the while condition is false
+		int i =6;
+		
+		do
+		{
+			System.out.println("hello " + i);
+			
+		}while(i<5);
+
+	}
+
+}
