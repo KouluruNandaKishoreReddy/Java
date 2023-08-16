83c30b30340a2a2a68a8e03c5fb16b628d80ac66
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-16 19:01:50 +0530

    Add files via upload

---
+++ Principle_caluclutor.java
@@ -0,0 +1,18 @@
+
+public class Principle_caluclutor {
+
+	public static void main(String[] args) {
+		
+		int p = 1_00_000;
+		double r= 0.5;
+		int t=3;
+		
+		double result = p*t*r ;
+		
+		System.out.println(result);
+		
+		
+
+	}
+
+}
