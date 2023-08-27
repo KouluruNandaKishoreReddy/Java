b9d25d6acd0952cb2bc9c92a0e286fd58ee9b414
KOULURU NANDA KISHORE REDDY <nandakouluruemail@gmail.com>
2023-08-27 14:54:38 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,22 @@
+
+
+class Hello
+{
+	public void nanda() {
+
+		System.out.println("Enter the name");
+	}
+	
+
+}
+public class Methods {
+
+	public static void main(String[] args) {
+		
+		Hello obj = new Hello ();
+		obj.nanda ();
+
+
+	}
+
+}
